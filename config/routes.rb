f25bccb195cdffd5449a32ef1dd31265fa4e1c44
MarekAG <marekwstrzala@gmail.com>
2015-05-16 16:57:40 +0200

    added static views, auth, users views and more

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  root                'static_pages#home'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users 
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'application#hello'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
