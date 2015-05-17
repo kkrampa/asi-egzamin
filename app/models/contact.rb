23e20d2fedceed4a44ee696d8a0cbf23a2d3a811
Kamil Krampa <kkrampa@soldevelo.com>
2015-05-17 14:46:19 +0200

    Added email validation in contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,7 @@
 class Contact < ActiveRecord::Base
+  validates :email, presence: false, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
