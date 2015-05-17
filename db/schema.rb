81acc8c3e1f9b98d919ce7c3b82742a6b7079d71
Kamil Krampa <kkrampa@soldevelo.com>
2015-05-17 14:27:52 +0200

    #2 Add contact model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413114503) do
+ActiveRecord::Schema.define(version: 20150517112552) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "phone_number"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
---
+++ test/test_helper.rb
@@ -0,0 +1,2 @@
+class TestHelper
+end
