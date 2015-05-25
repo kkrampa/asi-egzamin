de5394a195356fb20e2a43f9b88c6eae743f0ddf
Kamil Krampa <kkrampa@soldevelo.com>
2015-05-25 18:41:18 +0200

    Updated smpt credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'peaceful-shelf-3904.herokuapp.com'
+  host = 'salty-brook-9086.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
